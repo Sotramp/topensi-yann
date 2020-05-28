a15483db8e1ae9a61c3c9b5d25b747097bf426a3
hisakiyo <hisakiyo.games@gmail.com>
2020-05-28 17:03:48 +0200

    push

--- topensi/gestion/views.py
+++ topensi/gestion/views.py
@@ -39,3 +39,42 @@ class UpdateView(TemplateView):
     partenaire = Partenaire.objects.all()
     type = Type.objects.all()
     return render(request, self.template_name, {'info' : info, 'client': client, 'partenaire': partenaire, 'type' : type})
+
+class AjouterPartenaireView(TemplateView):
+  template_name = "index.html"
+  def post(self, request, **kwargs):
+    partenaire = request.POST.get('partenaire', False)
+    try:
+      p = Partenaire(nom=partenaire)
+      p.save()
+      messages.success(request, 'Le partenaire a bien été ajouté')
+      return HttpResponseRedirect( "/add/" )
+    except:
+      messages.error(request, "Impossible de créer un partenaire")
+      return HttpResponseRedirect( "/add/" )
+
+class AjouterClientView(TemplateView):
+  template_name = "index.html"
+  def post(self, request, **kwargs):
+    client = request.POST.get('client', False)
+    try:
+      c = Client(nom=client)
+      c.save()
+      messages.success(request, 'Le client a bien été ajouté')
+      return HttpResponseRedirect( "/add/" )
+    except:
+      messages.error(request, "Impossible de créer un client")
+      return HttpResponseRedirect( "/add/" )
+
+class AjouterTypeView(TemplateView):
+  template_name = "index.html"
+  def post(self, request, **kwargs):
+    type = request.POST.get('type', False)
+    try:
+      c = Type(nom=type)
+      c.save()
+      messages.success(request, 'Le type a bien été ajouté')
+      return HttpResponseRedirect( "/add/" )
+    except:
+      messages.error(request, "Impossible de créer un type")
+      return HttpResponseRedirect( "/add/" )
