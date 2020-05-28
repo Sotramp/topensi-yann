df0999e4980d998ca84ac7c2e564b5334a443d6c
hisakiyo <hisakiyo.games@gmail.com>
2020-05-28 16:17:31 +0200

    url.py

---
+++ topensi/gestion/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from lister.views import *
+
+urlpatterns = [
+    url(r'^$', LoginView.as_view()),
+]
