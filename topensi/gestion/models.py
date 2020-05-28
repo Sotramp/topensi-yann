e342ef4c0ff78900408a812d1171e1a44c32e8c0
boidart <boidatclement@gmail.com>
2020-05-28 16:16:58 +0200

    bdd

--- topensi/gestion/models.py
+++ topensi/gestion/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
 # Create your models here.
+class Client(models.Model):
+    nom = models.CharField(max_length=50, unique=True)
+
+class Partenaire(models.Model):
+    nom = models.CharField(max_length=50, unique=True)
+    
+class Type(models.Model):
+    nom = models.CharField(max_length=50, unique=True)
+
+class Info(models.Model):
+    cli = models.ForeignKey(Client, on_delete=models.CASCADE)
+    partenaire = models.ForeignKey(Partenaire, on_delete=models.CASCADE)
+    typ = models.ForeignKey(Type, on_delete=models.CASCADE)
+    marge = models.FloatField()
+    recurrent = models.FloatField()
+    etat = models.CharField(max_length=50)
+    facture = models.CharField(max_length=100)
+    dateCreation = models.DateField(max_length=100)
+    dateCloture = models.DateField(max_length=100)
