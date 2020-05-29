6ea7dddf85ab87c1e50925615b0c75a2e34e7cea
boidart <boidatclement@gmail.com>
2020-05-29 17:38:27 +0200

    ok

--- topensi/gestion/urls.py
+++ topensi/gestion/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     url(r'^add/$', AddView.as_view()),
     url(r'^update/$', UpdateView.as_view()),
     url(r'^recurrent/$', RecurrentView.as_view()),
-    url(r'^resume/$', ResumeView.as_view()),
     url(r'^add/ajouter_client/$', AjouterClientView.as_view()),
     url(r'^add/ajouter_type/$', AjouterTypeView.as_view()),
     url(r'^add/ajouter_partenaire/$', AjouterPartenaireView.as_view()),
--- topensi/gestion/views.py
+++ topensi/gestion/views.py
@@ -77,16 +77,6 @@ class UpdateView(TemplateView):
     type = Type.objects.all()
     return render(request, self.template_name, {'info' : info, 'client': client, 'partenaire': partenaire, 'type' : type, 'etat': etat})
 
-class ResumeView(TemplateView):
-  template_name = 'resume.html'
-  def get(self, request, **kwargs):
-    info = Info.objects.all()
-    client = Client.objects.all()
-    partenaire = Partenaire.objects.all()
-    etat = Etat.objects.all()
-    type = Type.objects.all()
-    return render(request, self.template_name, {'info' : info, 'client': client, 'partenaire': partenaire, 'type' : type, 'etat': etat})
-
 class AjouterPartenaireView(TemplateView):
   template_name = "index.html"
   def post(self, request, **kwargs):
